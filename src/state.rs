9390ab22de73e02fb903cbd172fc067b35e5536b
themicp <themicp@gmail.com>
2023-10-10 12:06:59 +0300

    Add state entry for forks info

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,10 @@
 use cw_storage_plus::{Item, Map};
 
-use crate::lightclient::types::{Bootstrap, ChainConfig, LightClientState, Update};
+use crate::lightclient::types::{Bootstrap, ChainConfig, Forks, LightClientState, Update};
 
 pub const UPDATES: Map<u64, Update> = Map::new("updates");
 
 pub const BOOTSTRAP: Item<Bootstrap> = Item::new("bootstrap");
 pub const CONFIG: Item<ChainConfig> = Item::new("config");
 pub const LIGHT_CLIENT_STATE: Item<LightClientState> = Item::new("light_client_state");
+pub const FORKS: Item<Forks> = Item::new("forks");
