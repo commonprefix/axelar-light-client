dbac5a54c19c4c46cc4477fc056c04501f121d0f
themicp <themicp@gmail.com>
2023-10-10 12:07:25 +0300

    Add forks in instantiate message, add 'forks' query

--- src/msg.rs
+++ src/msg.rs
@@ -1,12 +1,13 @@
 use cosmwasm_schema::cw_serde;
 use serde::{Deserialize, Serialize};
 
-use crate::lightclient::types::{Bootstrap, ChainConfig, Update};
+use crate::lightclient::types::{Bootstrap, ChainConfig, Forks, Update};
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub struct InstantiateMsg {
     pub bootstrap: Bootstrap,
     pub config: ChainConfig,
+    pub forks: Forks,
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
@@ -20,4 +21,5 @@ pub enum QueryMsg {
     Bootstrap {},
     Update { period: u64 },
     LightClientState {},
+    Forks {},
 }
