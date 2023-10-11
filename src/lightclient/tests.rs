248de7e90927dd8c677ab50e24e790e3d7cceae2
themicp <themicp@gmail.com>
2023-10-12 00:17:24 +0300

    Add tests for verify_update failures

--- src/lightclient/tests.rs
+++ src/lightclient/tests.rs
@@ -4,12 +4,13 @@ mod tests {
 
     use cosmwasm_std::testing::mock_env;
     use cosmwasm_std::Timestamp;
+    use ssz_rs::Bitvector;
 
     use crate::{
         lightclient::error::ConsensusError,
         lightclient::helpers::test_helpers::{get_bootstrap, get_config, get_update},
-        lightclient::types::{BLSPubKey, Header, SignatureBytes},
-        lightclient::{self, LightClient},
+        lightclient::types::{primitives::U64, BLSPubKey, SignatureBytes},
+        lightclient::{self, types::primitives::ByteVector, LightClient},
     };
 
     fn init_lightclient() -> LightClient {
@@ -33,31 +34,149 @@ mod tests {
     }
 
     #[test]
-    fn test_verify_update_invalid_committee() {
+    fn test_verify_update_insufficient_participation_error() {
         let lightclient = init_lightclient();
 
         let mut update = get_update(862);
-        update.next_sync_committee.pubkeys[0] = BLSPubKey::default();
+        update.sync_aggregate.sync_committee_bits = Bitvector::default();
 
         let err = lightclient.verify_update(&update).unwrap_err();
 
         assert_eq!(
             err.to_string(),
-            lightclient::ConsensusError::InvalidNextSyncCommitteeProof.to_string()
+            lightclient::ConsensusError::InsufficientParticipation.to_string()
         );
     }
 
     #[test]
-    fn test_verify_update_invalid_finality() {
+    fn test_verify_update_time_error() {
         let lightclient = init_lightclient();
 
         let mut update = get_update(862);
-        update.finalized_header.beacon = Header::default();
+        update.signature_slot = U64::from(
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs()
+                + 12,
+        );
+        let mut err = lightclient.verify_update(&update).unwrap_err();
 
-        let err = lightclient.verify_update(&update).err().unwrap();
         assert_eq!(
             err.to_string(),
-            ConsensusError::InvalidFinalityProof.to_string()
+            lightclient::ConsensusError::InvalidTimestamp.to_string()
+        );
+
+        update = get_update(862);
+        update.signature_slot = update.attested_header.beacon.slot;
+        err = lightclient.verify_update(&update).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidTimestamp.to_string()
+        );
+
+        update = get_update(862);
+        update.attested_header.beacon.slot =
+            U64::from(update.finalized_header.beacon.slot.as_u64() - 1);
+        err = lightclient.verify_update(&update).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidTimestamp.to_string()
+        );
+    }
+
+    #[test]
+    fn test_verify_update_period_error() {
+        let mut lightclient = init_lightclient();
+        // current period is 862, without a sync committee
+        let mut update = get_update(863);
+
+        let mut err = lightclient.verify_update(&update).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidPeriod.to_string()
+        );
+
+        // properly sync with period 862, store sync committee
+        update = get_update(862);
+        lightclient.apply_update(&update).unwrap();
+
+        // update was applied
+        assert!(lightclient.state.next_sync_committee.is_some());
+
+        // update period > current period + 1
+        update = get_update(864);
+        err = lightclient.verify_update(&update).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidPeriod.to_string()
+        );
+    }
+
+    #[test]
+    fn test_verify_update_relevance_error() {
+        let mut lightclient = init_lightclient();
+        let mut update = get_update(862);
+        lightclient.apply_update(&update).unwrap();
+
+        update.attested_header.beacon.slot = lightclient.state.finalized_header.slot;
+        update.finalized_header.beacon.slot = lightclient.state.finalized_header.slot;
+        assert!(lightclient.state.next_sync_committee.is_some());
+        let mut err = lightclient.verify_update(&update).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::NotRelevant.to_string()
+        );
+
+        update = get_update(862);
+        update.attested_header.beacon.slot =
+            U64::from(lightclient.state.finalized_header.slot.as_u64() - (256 * 32));
+        update.finalized_header.beacon.slot =
+            U64::from(lightclient.state.finalized_header.slot.as_u64() - (256 * 32));
+        lightclient.state.next_sync_committee = None;
+        err = lightclient.verify_update(&update).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::NotRelevant.to_string()
+        );
+    }
+
+    #[test]
+    fn test_verify_update_finaliy_proof() {
+        let lightclient = init_lightclient();
+        let mut update = get_update(862);
+
+        update.attested_header.beacon.state_root = ByteVector::default();
+        let mut err = lightclient.verify_update(&update).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidFinalityProof.to_string()
+        );
+
+        update = get_update(862);
+        update.finalized_header.beacon.state_root = ByteVector::default();
+        err = lightclient.verify_update(&update).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidFinalityProof.to_string()
+        );
+    }
+
+    #[test]
+    fn test_verify_update_invalid_committee() {
+        let lightclient = init_lightclient();
+
+        let mut update = get_update(862);
+        update.next_sync_committee.pubkeys[0] = BLSPubKey::default();
+        let err = lightclient.verify_update(&update).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            lightclient::ConsensusError::InvalidNextSyncCommitteeProof.to_string()
         );
     }
 
@@ -82,48 +201,6 @@ mod tests {
         let update = get_update(862);
         let res = lightclient.verify_update(&update);
         assert!(res.is_ok());
-    }
-
-    #[test]
-    fn test_verify_finality_invalid_finality() {
-        let lightclient = init_lightclient();
-
-        let mut update = get_update(862);
-        update.finalized_header.beacon = Header::default();
-
-        let err = lightclient.verify_update(&update).err().unwrap();
-        assert_eq!(
-            err.to_string(),
-            ConsensusError::InvalidFinalityProof.to_string()
-        );
-    }
-
-    #[test]
-    fn test_verify_finality_invalid_sig() {
-        let lightclient = init_lightclient();
-
-        let mut update = get_update(862);
-        update.sync_aggregate.sync_committee_signature = SignatureBytes::default();
-
-        let err = lightclient.verify_update(&update).err().unwrap();
-        assert_eq!(
-            err.to_string(),
-            ConsensusError::InvalidSignature.to_string()
-        );
-    }
-
-    #[test]
-    fn test_invalid_period() {
-        let lightclient = init_lightclient();
-
-        let update = get_update(863);
-        let err = lightclient.verify_update(&update).err().unwrap();
-
-        assert_eq!(
-            err.to_string(),
-            ConsensusError::InvalidPeriod.to_string(),
-            "should error on invalid period"
-        );
     }
 
     #[test]
