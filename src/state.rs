2e06e7384eab31414ab745b82c7ea1c4d0885bc3
themicp <themicp@gmail.com>
2023-10-11 13:37:48 +0300

    Store Contract Version in state

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,4 @@
+use cw2::ContractVersion;
 use cw_storage_plus::{Item, Map};
 
 use crate::lightclient::types::{ChainConfig, LightClientState, SyncCommittee};
@@ -5,3 +6,4 @@ use crate::lightclient::types::{ChainConfig, LightClientState, SyncCommittee};
 pub const CONFIG: Item<ChainConfig> = Item::new("config");
 pub const LIGHT_CLIENT_STATE: Item<LightClientState> = Item::new("light_client_state");
 pub const SYNC_COMMITTEES: Map<u64, SyncCommittee> = Map::new("sync_committees");
+pub const VERSION: Item<ContractVersion> = Item::new("contract_info");
