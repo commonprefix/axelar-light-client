2df65fb449c58622967887df403432829eeb5b34
Dimitris Lamprinos <pkakelas@gmail.com>
2023-11-01 17:55:07 +0200

    Implement wasm module in feeder

--- feeder/src/main.rs
+++ feeder/src/main.rs
@@ -1,31 +1,39 @@
 mod error;
 mod eth;
 mod types;
+mod wasm;
 
-use eth::{consensus::ConsensusRPC, execution::ExecutionRPC};
-use ethers::types::H256;
 use tokio;
+use wasm::WasmClient;
 
 #[tokio::main]
 async fn main() {
-    let url = "http://nimbus-mainnet.commonprefix.com";
-    let consensus = ConsensusRPC::new(url);
+    const NODE_URL: &str = "http://devnet.rpc.axelar.dev:9090";
+    const ADDR: &str = "axelar1mpk5vthrcgsugqtcn6ha8rt4uurpryrsnrezrxdmlxuqc6helu9qupm6nh";
 
-    let res = consensus.get_updates(863, 1).await.unwrap();
-    println!("Update: {:?}", res);
+    let mut wasm = WasmClient::new(NODE_URL.into(), ADDR.into());
+    // let state = wasm.get_state().await.unwrap();
+    // println!("State: {:?}", state);
+    let period = wasm.get_period().await.unwrap();
+    println!("Period: {:?}", period);
+    // let url = "http://nimbus-mainnet.commonprefix.com";
+    // let consensus = ConsensusRPC::new(url);
 
-    let url: &str = "https://eth.llamarpc.com";
-    let execution = ExecutionRPC::new(url).unwrap();
+    // let res = consensus.get_updates(863, 1).await.unwrap();
+    // println!("Update: {:?}", res);
 
-    let hash = "0x1f59181a06f73b58a02ffb2ec291e108af359f8c4702fb5c50d9e2d0a240ac43"
-        .parse::<H256>()
-        .unwrap();
+    // let url: &str = "https://eth.llamarpc.com";
+    // let execution = ExecutionRPC::new(url).unwrap();
 
-    let res = execution
-        .get_transaction_receipt(&hash)
-        .await
-        .unwrap()
-        .unwrap();
+    // let hash = "0x1f59181a06f73b58a02ffb2ec291e108af359f8c4702fb5c50d9e2d0a240ac43"
+    //     .parse::<H256>()
+    //     .unwrap();
 
-    println!("Transaction Receipt: {:?}", res);
+    // let res = execution
+    //     .get_transaction_receipt(&hash)
+    //     .await
+    //     .unwrap()
+    //     .unwrap();
+
+    // println!("Transaction Receipt: {:?}", res);
 }
