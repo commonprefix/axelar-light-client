eb9ea7369a6a9de32983d4b97a9404d5de3b8645
themicp <themicp@gmail.com>
2023-11-03 12:59:56 +0200

    Create state map to store pending messages

--- light-client/src/state.rs
+++ light-client/src/state.rs
@@ -1,3 +1,4 @@
+use connection_router::state::{CrossChainId, Message};
 use cw2::ContractVersion;
 use cw_storage_plus::{Item, Map};
 use types::{common::ChainConfig, consensus::SyncCommittee, lightclient::LightClientState};
@@ -6,3 +7,4 @@ pub const CONFIG: Item<ChainConfig> = Item::new("config");
 pub const LIGHT_CLIENT_STATE: Item<LightClientState> = Item::new("light_client_state");
 pub const SYNC_COMMITTEES: Map<u64, SyncCommittee> = Map::new("sync_committees");
 pub const VERSION: Item<ContractVersion> = Item::new("contract_info");
+pub const PENDING_MESSAGES: Map<CrossChainId, Message> = Map::new("pending_messages");
