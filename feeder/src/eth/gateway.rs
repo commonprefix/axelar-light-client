ab714a934baa4737db37770637841a4c1273bba1
Dimitris Lamprinos <pkakelas@gmail.com>
2023-11-15 13:11:29 +0200

    Implement interested messages in gateway

--- feeder/src/eth/gateway.rs
+++ feeder/src/eth/gateway.rs
@@ -1,3 +1,6 @@
+use crate::eth::constants::EXECUTION_RPC;
+use crate::eth::execution::ExecutionRPC;
+use crate::eth::utils::calc_slot_from_timestamp;
 use crate::types::Message;
 use ethers::abi::{Bytes, RawLog};
 use ethers::prelude::{EthEvent, Http};
@@ -6,6 +9,7 @@ use ethers::types::Filter;
 use ethers::types::{Address, Log, H256, U256};
 use eyre::Result;
 use std::sync::Arc;
+
 pub struct Gateway {
     client: Arc<Provider<Http>>,
     address: Address,
@@ -25,10 +29,10 @@ pub struct ContractCallWithToken {
 }
 
 impl Gateway {
-    pub fn new(endpoint: &str, address: &str) -> Self {
+    pub fn new(rpc: &str, address: &str) -> Self {
         let address = address.parse::<Address>().unwrap();
 
-        let provider = Provider::<Http>::try_from(endpoint).unwrap();
+        let provider = Provider::<Http>::try_from(rpc).unwrap();
         let client = Arc::new(provider);
 
         return Self { client, address };
@@ -91,5 +95,46 @@ impl Gateway {
         }
 
         Ok(events)
+    }
+
+    pub async fn get_messages_in_slot_range(
+        &self,
+        from_slot: u64,
+        to_slot: u64,
+    ) -> Result<Vec<Message>> {
+        // TODO: Move that out of the code
+        const BLOCK_RANGE: u64 = 10000;
+        let execution = ExecutionRPC::new(EXECUTION_RPC);
+        let latest_block_number = execution.get_latest_block_number().await?;
+
+        let messages = self
+            .get_contract_call_with_token_messages(
+                latest_block_number.as_u64() - BLOCK_RANGE,
+                latest_block_number.as_u64(),
+            )
+            .await?;
+
+        println!("All messages: {:?}", messages.len());
+        let block_heights = messages
+            .iter()
+            .map(|message| message.block_number)
+            .collect::<Vec<u64>>();
+
+        let blocks = execution.get_blocks(&block_heights).await.unwrap();
+
+        let filtered_messages: Vec<Message> = messages
+            .into_iter()
+            .zip(blocks.iter())
+            .filter_map(|(message, block)| {
+                block.as_ref().and_then(|block| {
+                    let slot = calc_slot_from_timestamp(block.timestamp.as_u64());
+                    (slot > from_slot && slot < to_slot).then(|| message)
+                })
+            })
+            .collect();
+
+        println!("Messages in range: {:?}", filtered_messages.len());
+
+        Ok(filtered_messages)
     }
 }
--- feeder/src/eth/mod.rs
+++ feeder/src/eth/mod.rs
@@ -1,2 +1,14 @@
+use self::consensus::ConsensusRPC;
+use self::execution::ExecutionRPC;
+use self::gateway::Gateway;
+
 pub mod consensus;
 pub mod execution;
+pub mod gateway;
+pub mod utils;
+
+pub mod constants {
+    pub const CONSENSUS_RPC: &str = "http://nimbus-mainnet.commonprefix.com";
+    pub const EXECUTION_RPC: &str = "https://eth.llamarpc.com";
+    pub const GATEWAY_ADDR: &str = "0x4F4495243837681061C4743b74B3eEdf548D56A5";
+}
