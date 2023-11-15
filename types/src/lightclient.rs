349ce31514aa6a9dff2ab9a0bfc40f02706094ad
Dimitris Lamprinos <pkakelas@gmail.com>
2023-11-15 14:00:23 +0200

    Add light client proof type

--- types/src/lightclient.rs
+++ types/src/lightclient.rs
@@ -1,10 +1,10 @@
-use crate::consensus::{BeaconBlockAlias, FinalityUpdate, OptimisticUpdate};
+use crate::consensus::{FinalityUpdate, OptimisticUpdate};
 use crate::helpers::{from_hex_string, to_hex_string};
-use crate::primitives::U64;
-use ssz_rs::Vector;
+use connection_router::state::Message;
+use ssz_rs::{Node, Vector};
 use sync_committee_rs::consensus_types::{BeaconBlockHeader, SyncAggregate, SyncCommittee};
 use sync_committee_rs::constants::{Root, SLOTS_PER_HISTORICAL_ROOT, SYNC_COMMITTEE_SIZE};
-use sync_committee_rs::types::AncestryProof;
+pub use sync_committee_rs::types::AncestryProof;
 
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug, Clone, Default)]
 pub struct LightClientState {
@@ -31,14 +31,31 @@ pub struct TopicInclusionRequest {
     pub topic: Vec<u8>,
 }
 
+#[derive(PartialEq, Debug, Clone)]
 pub enum UpdateVariant {
     Finality(FinalityUpdate),
     Optimistic(OptimisticUpdate),
 }
 
+#[derive(PartialEq, Debug, Clone)]
 pub struct EventVerificationData {
+    pub message: Message,
     pub update: UpdateVariant,
     pub target_block: BeaconBlockHeader,
     pub block_roots: Vector<Root, SLOTS_PER_HISTORICAL_ROOT>,
     pub ancestry_proof: AncestryProof,
+    pub receipt_proof: ReceiptProof,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug, Clone, Default)]
+pub struct ReceiptProof {
+    // Proof from receipt to receipts root
+    pub receipt_branch: Vec<Node>,
+    pub receipt_index: u64,
+    pub receipts_root: Root,
+    // Proof from receipts root to execution body
+    pub receipts_root_branch: Vec<Node>,
+    pub execution_payload_root: Root,
+    // Proof from execution payload to body_root
+    pub execution_payload_branch: Vec<Node>,
 }
