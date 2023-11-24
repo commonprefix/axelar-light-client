8c6a63194c6a7b0239c9482665ef2fd7e8e3181b
themicp <themicp@gmail.com>
2023-11-24 15:47:29 +0200

    Remove unsused types

--- feeder/src/prover/consensus.rs
+++ feeder/src/prover/consensus.rs
@@ -1,10 +1,13 @@
 use crate::error::RpcError;
 use crate::eth::{constants::STATE_PROVER_RPC, utils::get};
 use crate::prover::types::ProofResponse;
-use consensus_types::{consensus::BeaconStateType, proofs::AncestryProof};
+use consensus_types::consensus::{BeaconBlockAlias, BeaconStateType, BlockRootsType};
 use eyre::Result;
-use ssz_rs::{get_generalized_index, SszVariableOrIndex};
-use sync_committee_rs::constants::SLOTS_PER_HISTORICAL_ROOT;
+use ssz_rs::{get_generalized_index, Node, SszVariableOrIndex};
+use sync_committee_rs::{
+    constants::{BLOCK_ROOTS_INDEX, SLOTS_PER_HISTORICAL_ROOT},
+    types::{AncestryProof, BlockRootsProof},
+};
 
 /**
  * Generates a merkle proof from the transaction to the beacon block root.
