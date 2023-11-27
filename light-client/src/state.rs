f05bbe3086148a7969464e77aae3c447ee0c0e6f
themicp <themicp@gmail.com>
2023-11-27 17:34:28 +0200

    Create VERIFIED_MESSAGES state

--- light-client/src/state.rs
+++ light-client/src/state.rs
@@ -1,6 +1,7 @@
 use cw2::ContractVersion;
 use cw_storage_plus::{Item, Map};
 use sync_committee_rs::{consensus_types::SyncCommittee, constants::SYNC_COMMITTEE_SIZE};
+use types::lightclient::{CrossChainId, Message, MessageHash};
 use types::{common::ChainConfig, lightclient::LightClientState};
 
 pub const CONFIG: Item<ChainConfig> = Item::new("config");
@@ -8,3 +9,5 @@ pub const LIGHT_CLIENT_STATE: Item<LightClientState> = Item::new("light_client_s
 pub const SYNC_COMMITTEES: Map<u64, SyncCommittee<SYNC_COMMITTEE_SIZE>> =
     Map::new("sync_committees");
 pub const VERSION: Item<ContractVersion> = Item::new("contract_info");
+
+pub const VERIFIED_MESSAGES: Map<MessageHash, Message> = Map::new("sync_committees");
