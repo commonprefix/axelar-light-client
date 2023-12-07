5d2d083375a9e45fde4ddd4f5bc002132d36d513
Dimitris Lamprinos <pkakelas@gmail.com>
2023-12-07 13:38:33 +0200

    Remove main file from eth package

--- eth/src/main.rs
+++
@@ -1,9 +0,0 @@
-use eth::consensus::{ConsensusRPC, CustomConsensusApi};
-use eth::constants::CONSENSUS_RPC;
-
-#[tokio::main]
-async fn main() {
-	let consensus: ConsensusRPC = ConsensusRPC::new(CONSENSUS_RPC);
-	let res = consensus.get_latest_beacon_block().await;
-	println!("{:?}", res);
-}
