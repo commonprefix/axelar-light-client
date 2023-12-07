25003428a0d9cf7d43eebd0c9673a24f7e6ef76a
Dimitris Lamprinos <pkakelas@gmail.com>
2023-12-07 15:54:19 +0200

    Enhance consensus prover tests

--- prover/src/prover/consensus.rs
+++ prover/src/prover/consensus.rs
@@ -197,6 +197,7 @@ mod tests {
     };
     use crate::prover::mocks::mock_consensus_rpc::MockConsensusRPC;
     use crate::prover::mocks::mock_state_prover::MockStateProver;
+    use consensus_types::consensus::BeaconBlockAlias;
     use consensus_types::proofs::AncestryProof;
     use eth::consensus::EthBeaconAPI;
     use ssz_rs::{
@@ -205,17 +206,25 @@ mod tests {
     use sync_committee_rs::constants::SLOTS_PER_HISTORICAL_ROOT;
     use tokio::test as tokio_test;
 
-    /**
-     * TESTS BELOW REQUIRE NETWORK REQUESTS
-     */
-    #[tokio_test]
-    async fn test_transactions_proof() {
-        let consensus = &MockConsensusRPC::new();
+    async fn setup_block_and_provers(
+        consensus_block_number: u64,
+    ) -> (MockConsensusRPC, MockStateProver, BeaconBlockAlias, Node) {
+        let consensus = MockConsensusRPC::new();
         let state_prover = MockStateProver::new();
-        let mut block = consensus.get_beacon_block(7807119).await.unwrap();
+        let mut block = consensus
+            .get_beacon_block(consensus_block_number)
+            .await
+            .unwrap();
         let block_root = block.hash_tree_root().unwrap();
 
+        (consensus, state_prover, block, block_root)
+    }
+
+    #[tokio_test]
+    async fn test_transactions_proof_valid() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
         let tx_index = 15;
+
         let transaction = &mut block.body.execution_payload.transactions[tx_index];
         let node = transaction.hash_tree_root().unwrap();
 
@@ -235,12 +244,111 @@ mod tests {
     }
 
     #[tokio_test]
-    async fn test_receipts_root_proof() {
-        let consensus = &MockConsensusRPC::new();
-        let state_prover = MockStateProver::new();
-        let mut block = consensus.get_beacon_block(7807119).await.unwrap();
-        let block_root = block.hash_tree_root().unwrap();
+    async fn test_transactions_proof_invalid_transaction() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
+        let tx_index = 15;
 
+        let transaction = &mut block.body.execution_payload.transactions[tx_index];
+        transaction[0] = 0;
+
+        let node = transaction.hash_tree_root().unwrap();
+
+        let proof =
+            generate_transaction_proof(&state_prover, &block_root.to_string(), tx_index as u64)
+                .await
+                .unwrap();
+
+        let mut invalid_block_root = block_root.clone();
+        invalid_block_root.0[31] = 0;
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &node,
+            &proof.witnesses,
+            &GeneralizedIndex(proof.gindex as usize),
+            &invalid_block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_transactions_proof_wrong_transaction() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
+        let tx_index = 15;
+
+        // Different transaction
+        let transaction = &mut block.body.execution_payload.transactions[16];
+        let node = transaction.hash_tree_root().unwrap();
+
+        let proof =
+            generate_transaction_proof(&state_prover, &block_root.to_string(), tx_index as u64)
+                .await
+                .unwrap();
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &node,
+            &proof.witnesses,
+            &GeneralizedIndex(proof.gindex as usize),
+            &block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_transactions_proof_invalid_block_root() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
+
+        let tx_index = 15;
+        let transaction = &mut block.body.execution_payload.transactions[tx_index];
+        let node = transaction.hash_tree_root().unwrap();
+
+        let proof =
+            generate_transaction_proof(&state_prover, &block_root.to_string(), tx_index as u64)
+                .await
+                .unwrap();
+
+        let mut invalid_block_root = block_root.clone();
+        invalid_block_root.0[31] = 0;
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &node,
+            &proof.witnesses,
+            &GeneralizedIndex(proof.gindex as usize),
+            &invalid_block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_transactions_proof_invalid_proof() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
+        let tx_index = 15;
+        let transaction = &mut block.body.execution_payload.transactions[tx_index];
+        let node = transaction.hash_tree_root().unwrap();
+
+        let proof =
+            generate_transaction_proof(&state_prover, &block_root.to_string(), tx_index as u64)
+                .await
+                .unwrap();
+
+        let mut invalid_proof = proof.witnesses.clone();
+        invalid_proof[0] = Node::default();
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &node,
+            &invalid_proof,
+            &GeneralizedIndex(proof.gindex as usize),
+            &block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_receipts_root_proof_valid() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
         let proof = generate_receipts_root_proof(&state_prover, &block_root.to_string())
             .await
             .unwrap();
@@ -261,8 +369,94 @@ mod tests {
     }
 
     #[tokio_test]
-    async fn test_block_roots_proof() {
-        let consensus = &MockConsensusRPC::new();
+    async fn test_receipts_root_proof_invalid_proof() {
+        let (_, state_prover, mut block, block_root) = setup_block_and_provers(7807119).await;
+        let proof = generate_receipts_root_proof(&state_prover, &block_root.to_string())
+            .await
+            .unwrap();
+
+        let mut invalid_proof = proof.witnesses.clone();
+        invalid_proof[0] = Node::default();
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &block
+                .body
+                .execution_payload
+                .receipts_root
+                .hash_tree_root()
+                .unwrap(),
+            &invalid_proof,
+            &GeneralizedIndex(proof.gindex as usize),
+            &block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_receipts_root_proof_invalid_receipts_root() {
+        let (_, state_prover, _, block_root) = setup_block_and_provers(7807119).await;
+        let proof = generate_receipts_root_proof(&state_prover, &block_root.to_string())
+            .await
+            .unwrap();
+
+        let is_proof_valid = ssz_rs::verify_merkle_proof(
+            &Node::default(),
+            &proof.witnesses,
+            &GeneralizedIndex(proof.gindex as usize),
+            &block_root,
+        );
+
+        assert_eq!(is_proof_valid, false)
+    }
+
+    #[tokio_test]
+    async fn test_prove_ancestry() {
+        let (consensus, state_prover, _, _) = setup_block_and_provers(7807119).await;
+
+        let latest_block = consensus.get_beacon_block_header(7878867).await.unwrap();
+        let old_block_slot = 7878867 - 1000;
+        let proof = prove_ancestry_with_block_roots(
+            &state_prover,
+            &old_block_slot,
+            &latest_block.state_root.to_string(),
+        )
+        .await
+        .unwrap();
+
+        if let AncestryProof::HistoricalRoots {
+            block_root_proof: _,
+            block_summary_root: _,
+            block_summary_root_proof: _,
+            block_summary_root_gindex: _,
+        } = proof
+        {
+            panic!("Expected block roots proof")
+        }
+
+        let latest_block = consensus.get_beacon_block_header(7879376).await.unwrap();
+        let old_block_slot = 7870916 - 8196;
+        let proof = prove_ancestry_with_historical_summaries(
+            &consensus,
+            &state_prover,
+            &old_block_slot,
+            &latest_block.state_root.to_string(),
+        )
+        .await
+        .unwrap();
+
+        if let AncestryProof::BlockRoots {
+            block_roots_index: _,
+            block_root_proof: _,
+        } = proof
+        {
+            panic!("Expected historical summaries proof")
+        }
+    }
+
+    #[tokio_test]
+    async fn test_block_roots_proof_valid() {
+        let (consensus, _, _, _) = setup_block_and_provers(7807119).await;
         let latest_block = consensus.get_beacon_block_header(7878867).await.unwrap();
         let state_prover = MockStateProver::new();
         let mut old_block = consensus
@@ -296,7 +490,44 @@ mod tests {
     }
 
     #[tokio_test]
-    async fn test_historical_proof() {
+    async fn test_block_roots_proof_invalid_proof() {
+        let (consensus, state_prover, _, _) = setup_block_and_provers(7807119).await;
+        let latest_block = consensus.get_beacon_block_header(7878867).await.unwrap();
+        let mut old_block = consensus
+            .get_beacon_block_header(7878867 - 1000)
+            .await
+            .unwrap();
+
+        let proof = prove_ancestry_with_block_roots(
+            &state_prover,
+            &(old_block.slot as usize),
+            &latest_block.state_root.to_string(),
+        )
+        .await
+        .unwrap();
+
+        match proof {
+            AncestryProof::BlockRoots {
+                block_roots_index,
+                mut block_root_proof,
+            } => {
+                // Make proof invalid
+                block_root_proof[0] = Node::default();
+
+                let is_valid_proof = ssz_rs::verify_merkle_proof(
+                    &old_block.hash_tree_root().unwrap(),
+                    block_root_proof.as_slice(),
+                    &GeneralizedIndex(block_roots_index as usize),
+                    &latest_block.state_root,
+                );
+                assert_eq!(is_valid_proof, false)
+            }
+            _ => panic!("Expected block roots proof"),
+        }
+    }
+
+    #[tokio_test]
+    async fn test_historical_proof_valid() {
         let consensus = MockConsensusRPC::new();
         let state_prover = MockStateProver::new();
 
@@ -346,6 +577,64 @@ mod tests {
                     &block_summary_root,
                 );
                 assert!(is_valid_proof)
+            }
+            _ => panic!("Expected block roots proof"),
+        }
+    }
+
+    #[tokio_test]
+    async fn test_historical_proof_invalid_proofs() {
+        let (consensus, state_prover, _, _) = setup_block_and_provers(7807119).await;
+        let latest_block = consensus.get_beacon_block_header(7879376).await.unwrap();
+        let mut old_block = consensus
+            .get_beacon_block_header(7870916 - 8196)
+            .await
+            .unwrap();
+
+        let proof = prove_ancestry_with_historical_summaries(
+            &consensus,
+            &state_prover,
+            &(old_block.slot),
+            &latest_block.state_root.to_string(),
+        )
+        .await
+        .unwrap();
+
+        match proof {
+            AncestryProof::HistoricalRoots {
+                mut block_summary_root_proof,
+                mut block_root_proof,
+                block_summary_root_gindex,
+                block_summary_root,
+            } => {
+                // Make proofs invalid
+                block_summary_root_proof[0] = Node::default();
+                block_root_proof[0] = Node::default();
+
+                // Proof from state root to the specific block_summary_root of the historical_summaries
+                let is_valid_proof = ssz_rs::verify_merkle_proof(
+                    &block_summary_root,
+                    &block_summary_root_proof,
+                    &GeneralizedIndex(block_summary_root_gindex as usize),
+                    &latest_block.state_root,
+                );
+                assert_eq!(is_valid_proof, false);
+
+                // Proof from block_summary_root to the target block
+
+                let block_root_index = old_block.slot as usize % SLOTS_PER_HISTORICAL_ROOT;
+                let gindex = get_generalized_index(
+                    &Vector::<Node, SLOTS_PER_HISTORICAL_ROOT>::default(),
+                    &[SszVariableOrIndex::Index(block_root_index)],
+                );
+
+                let is_valid_proof = ssz_rs::verify_merkle_proof(
+                    &old_block.hash_tree_root().unwrap(),
+                    &block_root_proof,
+                    &GeneralizedIndex(gindex),
+                    &block_summary_root,
+                );
+                assert_eq!(is_valid_proof, false)
             }
             _ => panic!("Expected block roots proof"),
         }
