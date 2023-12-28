889a245c861bfda0943e62651ac526f044ee370a
Dimitris Lamprinos <pkakelas@gmail.com>
2023-12-28 17:35:47 +0200

    Remove needless imports

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -1,11 +1,4 @@
 pub mod prover;
 pub mod types;
 
-use eth::consensus::ConsensusRPC;
-use prover::{
-    consensus::ConsensusProver, execution::ExecutionProver, state_prover::StateProver,
-    types::ProverConfig,
-};
-use std::sync::Arc;
-
 pub use prover::Prover;
