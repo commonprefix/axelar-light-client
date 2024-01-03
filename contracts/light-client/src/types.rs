db02ca29bc52df4d7e6e88a1f86d0803bf70e2be
themicp <themicp@gmail.com>
2024-01-03 16:09:58 +0200

    Verification result is just a vector of tuples of strings

--- contracts/light-client/src/types.rs
+++ contracts/light-client/src/types.rs
@@ -1,8 +1,1 @@
-use types::lightclient::{CrossChainId, Hash};
-
-pub type VerificationResult = Vec<(ContentVariantId, String)>;
-#[derive(serde::Serialize, serde::Deserialize)]
-pub enum ContentVariantId {
-    CrossChainId(CrossChainId),
-    OperatorsHash(Hash),
-}
+pub type VerificationResult = Vec<(String, String)>;
