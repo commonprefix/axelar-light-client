8fc01dd38ec22a8ef3292ced9c17f73bd73e696d
themicp <themicp@gmail.com>
2024-01-05 18:02:59 +0200

    Add comments

--- types/src/proofs.rs
+++ types/src/proofs.rs
@@ -11,25 +11,11 @@ use sync_committee_rs::{
     constants::{Root, MAX_BYTES_PER_TRANSACTION},
 };
 
-#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-pub struct MessageProof {
-    // Proof from sync committee signed block to a recent block (either finalized or optimistic)
-    pub update: UpdateVariant,
-    // Proof from the recent block to the block that contains the event of the message
-    pub ancestry_proof: AncestryProof,
-    // The actual target block header
-    pub target_block: BeaconBlockHeader,
-    // Proof from the target block to the transaction that contains the event
-    pub transaction_proof: TransactionProof,
-    // Proof from the target block to the actual event
-    pub receipt_proof: ReceiptProof,
-}
-
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug, Clone, Eq)]
 pub enum UpdateVariant {
-    // LightClientFinalityUpdate from the beacon API spec.
+    /// LightClientFinalityUpdate from the beacon API spec.
     Finality(FinalityUpdate),
-    // LightClientOptimisticUpdate from the beacon API spec.
+    /// LightClientOptimisticUpdate from the beacon API spec.
     Optimistic(OptimisticUpdate),
 }
 
@@ -40,6 +26,9 @@ impl Default for UpdateVariant {
 }
 
 impl UpdateVariant {
+    /// Extracts the most recent (maybe) finalized block from the LightClientUpdate message.
+    /// In the case of a FinalityUpdate, we are using the most recent finalized block stored in finalized_header.
+    /// In the case of an OptimisticUpdate, we trust (optimistically) that the attested_header will be finalized.
     pub fn recent_block(&self) -> BeaconBlockHeader {
         match &self {
             UpdateVariant::Finality(update) => update.finalized_header.beacon.clone(),
@@ -48,25 +37,26 @@ impl UpdateVariant {
     }
 }
 
+/// Necessary proofs to verify that a given block is an ancestor of another block.
+/// In our case, it proves that the block that contains the event we are looking for, is an ancestor of the recent block that we got from the LightClientUpdate message.
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
 pub enum AncestryProof {
-    // This variant defines the proof data for a beacon chain header in the `state.block_roots`.
+    /// This variant defines the proof data to verify that a beacon chain block header is in the `state.block_roots` of another block.
     BlockRoots {
-        // Generalized index from the block_root that we care to the block_root to the state root.
+        /// Generalized index from a block_root that we care to the block_root to the state root.
         // No need to provide that, since it can be calculated on-chain.
         block_roots_index: u64,
         block_root_proof: Vec<Node>,
     },
-    // This variant defines the neccessary proofs for a beacon chain header in the
-    // `state.historical_roots`.
+    /// This variant defines the necessary proofs to verify that a beacon chain block header in the `state.historical_roots` of another block.
     HistoricalRoots {
-        // Proof for the target_block in the historical_summaries[index].block_summary_root
+        /// Proof for the target_block in the historical_summaries[index].block_summary_root
         block_root_proof: Vec<Node>,
-        // The hash of the specific block summary root that has the block
+        /// The hash of the specific block summary root that has the block
         block_summary_root: Root,
-        // Proof that historical_summaries[index].block_summary_root is in recent block state
+        /// Proof that historical_summaries[index].block_summary_root is in recent block state
         block_summary_root_proof: Vec<Node>,
-        // The generalized index for the historical_batch in state.historical_roots.
+        /// The generalized index for the historical_batch in state.historical_roots.
         block_summary_root_gindex: u64,
     },
 }
@@ -80,38 +70,42 @@ impl Default for AncestryProof {
     }
 }
 
+/// Proofs to verify that a transaction is part of a block
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug, Clone, Default)]
 pub struct TransactionProof {
-    // Same index of transaction to transaction trie and from receipt to receipt trie
     pub transaction_index: u64,
-    // Generalized index of transaction in target block
+    /// Generalized index of transaction in target block
     pub transaction_gindex: u64,
-    // Proof from target block to transaction
+    /// Proof that a transaction is part of the block's transactions trie
     pub transaction_proof: Vec<Node>,
-    // Actual transaction to keccak and test against tx_hash of message
+    /// Encoded transaction
     pub transaction: Transaction<MAX_BYTES_PER_TRANSACTION>,
 }
 
+/// Proofs to verify that a receipt is part of a block
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, Default)]
 pub struct ReceiptProof {
-    // Proof from receipts root to beacon block
+    /// Proof that a receipts_root is part of a block
     pub receipts_root_proof: Vec<Node>,
-    // Proof from receipt to receipts root trie
+    /// Proof that a receipt is part of the receipts_root
     #[serde(
         serialize_with = "hex_array_serializer",
         deserialize_with = "hex_array_deserializer"
     )]
     pub receipt_proof: Vec<Vec<u8>>,
-    // Receipts root of execution payload of target block
+    /// The root of the receipts trie
     pub receipts_root: Root,
 }
 
+/// High-level structure that contains the messages that need verification, along with the necessary proofs
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct BatchVerificationData {
     pub update: UpdateVariant,
     pub target_blocks: Vec<BlockProofsBatch>,
 }
 
+/// Batch containing the proofs and messages to verify from a specific block.
+/// Each block might have multiple transactions with multiple messages for verification.
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct BlockProofsBatch {
     pub ancestry_proof: AncestryProof,
@@ -119,11 +113,12 @@ pub struct BlockProofsBatch {
     pub transactions_proofs: Vec<TransactionProofsBatch>,
 }
 
+/// Batch containing the proofs and messages to verify from a specific transaction.
+/// Each transaction might have multiple messages for verification.
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct TransactionProofsBatch {
     pub transaction_proof: TransactionProof,
     pub receipt_proof: ReceiptProof,
-    // Support multiple messages on a single tx, ie transaction level batching
     pub content: Vec<ContentVariant>,
 }
 
