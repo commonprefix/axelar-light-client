f74aa84d38e2eb46cdcd28f1dee02beabe5f9616
Dimitris Lamprinos <pkakelas@gmail.com>
2024-01-08 21:04:52 +0200

    Update feeder to use the verifier API properly

--- relayer/src/bin/feed.rs
+++ relayer/src/bin/feed.rs
@@ -4,9 +4,8 @@ use eth::{
     consensus::{ConsensusRPC, EthBeaconAPI},
     types::EthConfig,
 };
-use relayer::{load_config, verifier::Verifier};
+use relayer::{utils::{load_config, calc_sync_period}, verifier::Verifier};
 use std::{sync::Arc, time::Duration};
-use sync_committee_rs::constants::SLOTS_PER_EPOCH;
 use tokio::time::interval;
 
 const MAX_UPDATES_PER_LOOP: u8 = 100;
@@ -20,18 +19,36 @@ async fn main() {
     let consensus = Arc::new(ConsensusRPC::new(config.consensus_rpc.clone(), eth_config));
     let mut verifier = Verifier::new(config.wasm_rpc, config.verifier_addr);
 
-    loop {
-        interval.tick().await; // This should go first.
 
-        let period = verifier.get_period().await;
-        if period.is_err() {
-            println!("Error getting period from wasm: {:?}", period);
+    loop {
+        interval.tick().await;
+
+        let latest_header = consensus.get_latest_beacon_block_header().await;
+        if latest_header.is_err() {
+            println!("Error getting latest header from consensus: {:?}", latest_header.err());
             continue;
         }
-        let period = period.unwrap();
+        let latest_header = latest_header.unwrap();
+
+        let latest_period = calc_sync_period(latest_header.slot);
+        let verifier_period = verifier.get_period().await;
+        if verifier_period.is_err() {
+            println!("Error getting period from wasm: {:?}", verifier_period);
+            continue;
+        }
+        let verifier_period = verifier_period.unwrap();
+
+        println!(
+            "Latest period: {}, Verifier period: {}",
+            latest_period, verifier_period
+        );
+        if latest_period == verifier_period {
+            println!("No updates to process");
+            continue;
+        }
 
         let updates = consensus
-            .get_updates(period + 1, MAX_UPDATES_PER_LOOP)
+            .get_updates(verifier_period + 1, MAX_UPDATES_PER_LOOP)
             .await;
         if updates.is_err() {
             println!("Error getting updates from consensus: {:?}", updates.err());
@@ -42,19 +59,21 @@ async fn main() {
             println!("No updates to process");
             continue;
         }
-        let first_update_period = updates[0].attested_header.beacon.slot / SLOTS_PER_EPOCH / 256;
         println!(
             "Processing {} updates starting from period {}",
             updates.len(),
-            first_update_period
+            verifier_period + 1
         );
 
         for update in updates {
+            let update_period = calc_sync_period(update.attested_header.beacon.slot);
             let result = verifier.update(update).await;
             if result.is_err() {
                 println!("Error updating wasm: {:?}", result.err());
                 break;
             }
+
+            println!("Update {} successfully", update_period);
         }
     }
 }
--- relayer/src/lib.rs
+++ relayer/src/lib.rs
@@ -2,7 +2,5 @@ pub mod consumers;
 pub mod parser;
 pub mod relayer;
 pub mod types;
-mod utils;
+pub mod utils;
 pub mod verifier;
-
-pub use utils::load_config;
